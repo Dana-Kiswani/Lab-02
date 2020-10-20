f96b39ef1f90b498851b50942a0dd5a821292666
Dana Kiswani <danakiswani0@gmail.com>
2020-10-20 13:07:13 +0300

    leb-02-edits

--- js/app.js
+++ js/app.js
@@ -1,65 +1,63 @@
 'use strict';
-// $('document').ready(function(){
 
 
 //array to hold every this. on the const
 let kolshi = [];
 
-// Constructor Building
-function AnimalLand(name) {
-  this.title = name.title;
-  this.image_url = name.image_url;
-  this.description = name.description;
-  this.keyword = name.keyword;
-  this.horns = name.horns;
-  //---
-  kolshi.push(this);
+//ready function
+$('document').ready(function () {
 
-}
 
-// ----------🍟🍟🍟🍟🍟🍟🍟-------------
-//render function
+  // Constructor Building
+  function AnimalLand(name) {
+    this.title = name.title;
+    this.image_url = name.image_url;
+    this.description = name.description;
+    this.keyword = name.keyword;
+    this.horns = name.horns;
+    //---
+    kolshi.push(this);
+  }
 
-AnimalLand.prototype.picViewer = function(){
-  let section = $('.photo-template').clone();
-  let Options = $('<option></option>').text(this.keyword);
-  $('select').append(Options);
-  //--
-  $('main').append(section);
-  section.find('h2').text(this.title);
-  section.find('img').attr('src', this.image_url);
-  section.find('p').text(this.description);
-  section.find('span').text(this.keyword);
-  section.find('div').text(this.horns);
-  section.removeClass('photo-template');
-};
+  // ----------🍟🍟🍟🍟🍟🍟🍟-------------
+  //render function
 
-// ---------🥰🥰🥰🥰🥰🥰-----------------
-//ajax arrow function
+  AnimalLand.prototype.picViewer = function () {
+    let section = $('.photo-template').clone();
+    let Options = $('<option></option>').text(this.keyword);
+    $('select').append(Options);
+    //--
+    $('main').append(section);
+    section.find('h2').text(this.title);
+    section.find('img').attr('src', this.image_url);
+    section.find('p').text(this.description);
+    section.find('.p1').text(this.keyword)
+    section.find('div').text(`Horns Number: ${this.horns}`);
+    section.attr('class', this.keyword);
 
-AnimalLand.readJson = () => {
+  };
+
+  // ---------🥰🥰🥰🥰🥰🥰-----------------
+  //ajax arrow function
+
   const ajaxSettings = {
     method: 'get',
     dataType: 'json'
   };
-    //----
-  $.ajax('../data/page-1.json', ajaxSettings).then(data =>{
+  //----
+  $.ajax('../data/page-1.json', ajaxSettings).then(data => {
     data.forEach(dana => {
-      let buildo = new AnimalLand(dana);
-      buildo.picViewer();
+      let buildup = new AnimalLand(dana);
+      buildup.picViewer();
     });
   });
-};
 
-// -------------☎📞☎📞☎📞☎📞---------------
-//calling ajax
-$(() => AnimalLand.readJson());
 
-//---
-
-$(document).ready(function () {
-  $('select').on('change', function() {
+  // -------------☎📞☎📞☎📞☎📞---------------
+  //---
+  $('select').on('change', function () {
     let selected = this.value;
+    $('section').hide();
     $(`.${selected}`).show();
   });
 });
